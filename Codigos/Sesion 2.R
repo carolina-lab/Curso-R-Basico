dd99353f292de5437c415f4b6297cff367aca79b
Laboratorio Fintrade <60900100+Fintrade2020@users.noreply.github.com>
2020-07-02 22:02:16 -0500

    CODIGOS

---
+++ Codigos/Sesion 2.R
@@ -0,0 +1,31 @@
+# Sesion 2 Curso Basico R
+
+rm(list=ls())
+
+##########################################################
+#############   Martes 14 de Julio 2020   ################
+#############   Laboratorio Fintrade           ################
+#############                             ################
+##########################################################
+
+
+## Contenido
+
+# Ubicando dirección URL
+# Tipos de Archivos
+# Libreria dplyr
+# Bases de datos variables categóricas
+# Bases de datos Numéricas
+
+library(AER)
+data("TravelMode")
+data("Fatalities")
+
+
+library (xlsx)
+Write.xlsx (FaFatalities, file = "base1.xlsx",
+            SheetName = "Accidentes de EE. UU.")
+
+
+
+
